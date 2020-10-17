b7754d6862f96731abe54b36739a794b00ff3422
Leeroy Phili <leeroyphili@gmail.com>
2020-10-17 10:13:13 -0400

    connect the navigation set up to the app

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
-import {createStackNavigator, creatAppContainer} from "react-navigation";
-import IndexScreen from './src/screens/IndexScreen'
+import React from 'react'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from "./src/screens/IndexScreen";
 
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: 'Blogs'      
+    }
+  }
+);
 
-const navigation 
+const App = createAppContainer (navigator)
+
+export default () => {
+  return <App/>
+};
