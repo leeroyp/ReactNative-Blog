8a343e600d7d14523cf015d4de0bc1aee1479dd6
Leeroy Phili <leeroyphili@gmail.com>
2020-10-17 09:45:18 -0400

    added a new index screen file

--- App.js
+++ App.js
@@ -1,21 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator, creatAppContainer} from "react-navigation";
+import IndexScreen from './src/screens/IndexScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const navigation 
