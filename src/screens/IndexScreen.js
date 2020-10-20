5f60452eabbc88946c790d4d3aea3d0b63f718f1
Leeroy Phili <leeroyphili@gmail.com>
2020-10-20 13:11:44 -0400

    Remote fetch of the posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import {
   View,
   Text,
@@ -11,8 +11,12 @@ import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({navigation}) => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost, getBlogPosts} = useContext(Context);
 
+  useEffect(()=> {
+    getBlogPosts();
+  }, [])
+  
   return (
     <View>
       <FlatList
